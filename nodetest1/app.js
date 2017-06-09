1f91ec001bc5aef08c65d7803d92bf7315ef106f
Michaela Davis <michaela.delight@gmail.com>
2017-06-09 10:24:45 -0700

    :books: add README

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -1,3 +1,6 @@
+// The Dead-Simple Step-By-Step Guide for Front-End Developers to Getting Up and Running With Node.JS, Express, and MongoDB
+// https://closebrace.com/tutorials/2017-03-02/the-dead-simple-step-by-step-guide-for-front-end-developers-to-getting-up-and-running-with-nodejs-express-and-mongodb
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
